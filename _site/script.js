4c08354de8d5f0b042380e8d49cdcf2874ddd5f5
axchristie <alex.christie.88@gmail.com>
2015-04-17 15:30:23 -0700

    bottom-header experimenting

--- _site/script.js
+++ _site/script.js
@@ -91,8 +91,9 @@ var interactive_bg = function(strength){
         $('.content').addClass('little_pad');
         
         $('header').addClass('sticky');
-        $('#bottom-header').css('font-size', '125%');
-        $('#bottom-header').css('line-height', '1em');        
+        $('#bottom-header').css('font-size', '1.5em');
+        $('#bottom-header').css('line-height', '1em');
+        $('#bottom-header').css('overflow', 'contain');
         
         $('#slow').hide();
         $('#fast').show();
