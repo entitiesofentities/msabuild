6c6f064b2889437c83be80767c942e0ab0dc32f3
axchristie <alex.christie.88@gmail.com>
2020-03-05 20:03:58 -0500

    begin reworking sketch

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup() {
   img = loadImage('assets/manhole.png');
   walkman = loadImage('assets/walkman.png');
   bg = loadImage('assets/bg-img.jpg');
-  arenoFont = loadFont('fonts/Lato-Light.ttf')
+  arenoFont = loadFont('fonts/Areno-Regular.ttf')
   // Move the canvas so it’s inside our <div id="sketch-holder">.
   canvas.parent('sketch-holder');
   centerCanvas();
@@ -44,22 +44,25 @@ rect(0, 0, gutterSize, height);
 rect(width - gutterSize, 0, width, height);
 fill(255);
 
-crosswalkVertical()
+crosswalkHorizontal()
 walkSign();
 fill(255);
 //text(mouseY, 50, 100);
 
-if (mouseY < 599 && millis() > 2000) {
+if (mouseY < 599) {
 
 //crosswalkBG();
 
 fill(255);
-text("STREETS", map(mouseY, 0, 1000, -500, gutterSize + 1000), height/2 + 150);
+textSize(150);
+text("MSA", 840 - mouseY, 390);
+text("STREETS", 1490 - mouseY, 390);
 fill(255);
 
 //crosswalkFG();
 
-translate(gutterSize + 400, map(mouseY, 0, 1000, 800, -250));
+//translate(gutterSize + 400, map(mouseY, 0, 1000, 800, -250));
+translate(width - gutterSize - mouseY - 300, 325);
 rotate(map(mouseY, 0, 1000, 0, 10));
 image(img, 0, 0, img.width/2, img.height/2);
 }
@@ -68,11 +71,16 @@ if (mouseY > 599 && millis() > 2000) {
 //crosswalkBG();
 //crosswalkFG();
 
-fill(255);
-text("STREETS", map(mousePos, 0, 1000, -500, gutterSize + 1000), height/2 + 150);
 
-translate(gutterSize + 400, map(mousePos, 0, 1000, 800, -250));
-rotate(map(mousePos, 0, 1000, 0, 10));
+textSize(150);
+fill('aqua');
+text("MSA", 840 - mouseY, 390);
+fill('red');
+text("STREETS", 1490 - mouseY, 390);
+fill(255);
+
+translate(width - gutterSize - mouseY - 300, 325);
+rotate(0);
 image(img, 0, 0, img.width/2, img.height/2);
 }
 
@@ -88,6 +96,24 @@ function crosswalkBG(){
   for (i = 150; i < width; i += 300) {
     rect(20 + i, 20, 50, height - 20);
   }
+}
+
+function crosswalkHorizontal(){
+
+  for (i = -1000; i < 1450; i += 80) {
+    fill(255);
+    xPos = 20 + i - mouseY;
+    if (i < 800 || i > 1000) {
+    rect(20 + i - mouseY, 250, 40, 150);
+  }
+  }
+
+  for (i = 2000; i < 5000; i += 80) {
+    fill(255);
+    xPos = 20 + i - mouseY;
+    rect(20 + i - mouseY, 250, 40, 150);
+  }
+
 }
 
 function crosswalkVertical(){
@@ -108,21 +134,25 @@ if (mouseY > 600) {
 
 function walkSign(){
   fill(100);
-
+  textSize(50);
   if (millis() < 2000) {
-  rect(gutterSize + 250, height/2 - 150, 300, 300);
+  rect(gutterSize + 250, height/2 - 150, 150, 150);
   fill('orange');
-  text ("WAIT", gutterSize + 260, height/2 - 50);
+  text ("WAIT", gutterSize + 260, height/2 - 80);
   fill('black');
-  text ("WALK", gutterSize + 260, height/2 + 100);
+  text ("WALK", gutterSize + 260, height/2 - 20);
+  fill(50);
+  rect(gutterSize + 305, height/2, 25, height);
 }
 
 if (millis() > 2000) {
-  rect(gutterSize + 250, height/2 - 150 - mouseY, 300, 300);
+  rect(gutterSize + 250, height/2 - 150, 150, 150);
   fill('black');
-  text ("WAIT", gutterSize + 260, height/2 - 50 - mouseY);
+  text ("WAIT", gutterSize + 260, height/2 - 80);
   fill('green');
-  text ("WALK", gutterSize + 260, height/2 + 100 - mouseY);
+  text ("WALK", gutterSize + 260, height/2 - 20);
+  fill(50);
+  rect(gutterSize + 305, height/2, 25, height);
 }
 
 }
